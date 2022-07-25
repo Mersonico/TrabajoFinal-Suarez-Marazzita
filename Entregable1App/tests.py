fb4961b372c017292b8de8ce10e763c137107d31
Mersonico <cristianmarazzita@hotmail.com>
2022-07-24 22:36:04 -0300

    Update tests.py

--- Entregable1App/tests.py
+++ Entregable1App/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Cancha
+
+class CanchasTest(TestCase):
+    
+    def setUp(self):
+        Cancha.objects.create(tipo="Cristian", tamaño=2)
+        
+    def test_cancha_nombre(self):
+        chancha = Cancha.objects.get(tamaño=2)
+        self.assertEqual(Cancha.nombre, "Cristian")
+    
+    def test_cancha_creado(self):
+        cancha = Cancha.objects.get(tamaño=2)
+        self.assertNotEquals(cancha, None)
+        
